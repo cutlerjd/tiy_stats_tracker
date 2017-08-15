bc69a9bcd9e7b1da5f3d0ea2baf85739e0925853
Jase Cutler <jasecutler@gmail.com>
2017-08-15 15:58:35 -0700

    Cleanup of unnecessary dependencies

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser')
+
+const Authorize = require('./middleware/authorization.js')
+
+//My routes
 const indexRouter = require('./routes/indexRoute')
 const aboutRouter = require('./routes/aboutRoute')
-const session = require('express-session')
-const bodyParser = require('body-parser')
+const apiRouter = require('./routes/apiRoute')
+const loginRoute = require('./routes/loginRoute')
 
 //Basic body parser settings
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-//Rendering with mustache
-app.engine('mustache', mustacheExpress());
-app.set('views', './views')
-app.set('view engine', 'mustache')
-
-//Used for session managment
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
-
-//Allows for a static directory for hosting CSS and other files
-app.use(express.static(path.join(__dirname, 'static')))
-
 //Use the routes files.
 app.use('/', indexRouter);
-app.use('/about', aboutRouter);
+app.use('/1/api',loginRoute)
+app.use('/1/api',Authorize,apiRouter)
 
 app.listen(3000, function(){
   console.log("App running on port 3000")
