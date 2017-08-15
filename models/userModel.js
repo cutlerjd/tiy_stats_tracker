dc469a8801bd147d2af90ef03b510c5162654f8c
Jase Cutler <jasecutler@gmail.com>
2017-08-15 16:01:00 -0700

    User login register created

---
+++ models/userModel.js
@@ -0,0 +1,142 @@
+const conn = require('../lib/db.js')
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid')
+
+function createUser(username, password, displayName) {
+    console.log(username, password, displayName)
+    return new Promise(function (resolve, reject) {
+        const hash = bcrypt.hash(password, 8).then(function (hash) {
+            let sql = `
+            INSERT INTO users (username,displayName,passwordHash)
+            VALUES (?,?,?)
+            `
+            conn.query(sql, [username, displayName, hash], function (err, results, fields) {
+                if (err) {
+                    reject({
+                        status: 'Failure',
+                        message: 'createUser DB Failure'
+                    })
+                } else {
+                    let token = uuid()
+                    let sql2 = `
+                    INSERT INTO tokens (token,id_user)
+                    VALUES (?,?)`
+                    conn.query(sql2, [token, results.insertId], function (err, results, fields) {
+                        if (err) {
+                            reject({
+                                status: 'Failure',
+                                message: 'createUser failed to store token in DB'
+                            })
+                        } else {
+                            resolve({
+                                status: 'Success',
+                                userInfo: {
+                                    username: username,
+                                    displayName: displayName,
+                                    token: token
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        })
+            .catch(function (data) {
+                console.log(data)
+                reject({
+                    status: 'Failure',
+                    message: 'createUser failed to create hash'
+                })
+            })
+    })
+}
+
+function getNewToken(username, password) {
+    console.log(username)
+    return new Promise(function (resolve, reject) {
+        let sql = `
+        SELECT *
+        FROM users
+        WHERE username = ?`
+        conn.query(sql, [username], function (err, results, fields) {
+            if (err) {
+                reject({
+                    status: 'Failure',
+                    message: 'getNewToken failed first DB query'
+                })
+            } else {
+                if(!results[0]){
+                    reject({
+                        status:'Failure',
+                        message:'Invalid credentials'
+                    })
+                } else {
+                passwordCompare = bcrypt.compare(password, results[0].passwordHash)
+                    .then(function () {
+                        let token = uuid()
+                        let sql2 = `
+                                    INSERT INTO tokens (token,id_user)
+                                    VALUES (?,?)`
+                        conn.query(sql2, [token, results[0].id_user], function (err, results2, fields) {
+                            if (err) {
+                                reject({
+                                    status: 'Failure',
+                                    message: 'getNewToken failed to store token in DB'
+                                })
+                            } else {
+                                resolve({
+                                    status: 'Success',
+                                    userInfo: {
+                                        username: username,
+                                        displayName: results[0].displayName,
+                                        token: token
+                                    }
+                                })
+                            }
+                        })
+                    })
+                    .catch(function(){
+                        reject({
+                            status:'Failure',
+                            message:'Invalid credentials'
+                        })
+                    
+                    })
+                }
+            }
+        })
+    })
+}
+function verifyToken(token){
+    return new Promise(function(resolve,reject){
+        let sql = `
+        SELECT token
+        FROM tokens
+        WHERE token = ?`
+        conn.query(sql,[token],function(err,results,fields){
+            if(err){
+                reject({
+                    status:'Failure',
+                    message:'verifyToken failed db query'
+                })
+            }else {
+                if(!results[0]){
+                    reject({
+                        status:'Failure',
+                        message:'Valid token not present'
+                    })
+                }else{
+                    resolve(true)
+                }
+            }
+        })
+    })
+}
+verified = verifyToken('9914d8a6-1d6d-43f0-9e0b-7d29ef3a8125')
+verified.then(console.log)
+.catch(console.log)
+module.exports = {
+    createUser: createUser,
+    getNewToken: getNewToken,
+    verifyToken:verifyToken
+}
---
+++ routes/apiRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router()
+
+// GET	/activities	Show a list of all activities I am tracking, and links to their individual pages
+router.get('/activities', function(req,res,next){
+    res.json({
+        message:'Insert activites here'
+    })
+})
+// POST	/activities	Create a new activity for me to track.
+// GET	/activities/{id}	Show information about one activity I am tracking, and give me the data I have recorded for that activity.
+// PUT	/activities/{id}	Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
+// DELETE	/activities/{id}	Delete one activity I am tracking. This should remove tracked data for that activity as well.
+// POST	/activities/{id}/stats	Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
+// DELETE	/stats/{id}	Remove tracked data for a day.
+
+module.exports = router;
