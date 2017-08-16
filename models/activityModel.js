383a1124ca32ad8a156909f5e9836c5ad7feaa9e
Jase Cutler <jasecutler@gmail.com>
2017-08-15 23:12:27 -0700

    Activity actions updated

--- models/activityModel.js
+++ models/activityModel.js
@@ -6,7 +6,7 @@ function getActivities(id_user){
         select  COUNT(s.id_act) as Count, s.id_user, s.id_act, a.type, a.name, DATE(s.timestamp) as DATE
         FROM stats s
         LEFT JOIN activities a ON s.id_act=a.id_act
-        WHERE s.id_user = ?
+        WHERE s.id_user = ? AND a.active = 1
         group by DATE, id_act`
         conn.query(sql,[id_user],function(err,results,fields){
             if(err){
@@ -23,7 +23,85 @@ function getActivities(id_user){
         })
     })
 }
-
+function getActivity(id_user,id_act){
+    return new Promise(function(resolve,reject){
+        let sql = `
+        select  COUNT(s.id_act) as Count, s.id_user, s.id_act, a.type, a.name, DATE(s.timestamp) as DATE
+        FROM stats s
+        LEFT JOIN activities a ON s.id_act=a.id_act
+        WHERE s.id_user = ? AND s.id_act = ? AND a.active = 1
+        group by DATE, id_act`
+        conn.query(sql,[id_user,id_act],function(err,results,fields){
+            if(err){
+                reject({
+                    status:'Failure',
+                    message: 'getActivities had a DB error'
+                })
+            } else{
+                resolve ({
+                    status:'Success',
+                    activities: results
+                })
+            }
+        })
+    })
+}
+function createActivity(id_user,act_name,act_type){
+    return new Promise(function(resolve,reject){
+        let sql = `
+        INSERT INTO activities (name,type,id_user)
+        VALUES (?,?,?)`
+        conn.query(sql,[act_name,act_type,id_user],function(err,results,fields){
+            if(err){
+                reject({
+                    status:'Failure',
+                    message: 'createActivity had a DB error'
+                })
+            } else {
+                resolve ({
+                    status:'Success',
+                    activity: {
+                        id_act:results.insertId,
+                        name:act_name,
+                        type:act_type,
+                        id_user: id_user
+                    }
+                })
+            }
+        })
+    })
+}
+function putActivity(id_user,id_act){
+    return new Promise(function(resolve,reject){
+        let sql = `
+        INSERT INTO stats (id_user,id_act)
+        VALUES (?,?)`
+        conn.query(sql,[id_user,id_act],function(err,results,fields){
+            if(err){
+                reject ({
+                    status:'Failure',
+                    message:'putActivity db error'
+                })
+            } else {
+                resolve ({
+                    status:'Success',
+                    id_act:id_act
+                })
+            }
+        })
+    })
+}
+function deleteActivity(id_user,id_act){
+    return new Promise(function(resolve,reject){
+        let sql = `
+        UPDATE activities
+        SET active = 0
+        WHERE id_user = ? AND id_act = ?`
+    })
+}
 module.exports = {
-    getActivities: getActivities
+    getActivities: getActivities,
+    getActivity: getActivity,
+    createActivity: createActivity,
+    putActivity:putActivity
 }
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -13,8 +13,39 @@ router.get('/activities', function(req,res,next){
     })
 })
 // POST	/activities	Create a new activity for me to track.
+router.post('/activities', function(req,res,next){
+    let act_name = req.body.name
+    let act_type = req.body.type
+    let activity = Activity.createActivity(res.locals.id_user,act_name,act_type)
+    activity.then(function(data){
+        res.json(data)
+    })
+    .catch(function(data){
+        res.json(data)
+    })
+})
 // GET	/activities/{id}	Show information about one activity I am tracking, and give me the data I have recorded for that activity.
+router.get('/activities/:id', function(req,res,next){
+    let id_act = req.params.id
+    let activity = Activity.getActivity(res.locals.id_user,id_act)
+    activity.then(function(data){
+        res.json(data)
+    })
+    .catch(function(data){
+        res.json(data)
+    })
+})
 // PUT	/activities/{id}	Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
+router.put('/activities/:id', function(req,res,next){
+    let id_act = req.params.id
+    let activity = Activity.putActivity(res.locals.id_user,id_act)
+    activity.then(function(data){
+        res.json(data)
+    })
+    .catch(function(data){
+        res.json(data)
+    })
+})
 // DELETE	/activities/{id}	Delete one activity I am tracking. This should remove tracked data for that activity as well.
 // POST	/activities/{id}/stats	Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
 // DELETE	/stats/{id}	Remove tracked data for a day.
